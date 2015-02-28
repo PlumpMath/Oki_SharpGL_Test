eb7c7eb941486b7671a4c9d367af8c743e902398
Okulich <ivanokulichev@gmail.com>
2015-02-28 21:27:34 +0100

    commit

---
+++ TestSharpGL/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using SharpGL;
+using SharpGL.SceneGraph.Assets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestSharpGL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private float rtri = 0;
+        private float rquad = 0;
+
+        private void OpenGLControl_OpenGLDraw(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            OpenGL gl = this.OpenGLControl1.OpenGL;
+
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+
+            gl.Translate(-1.5f, 0.0f, -6.0f);   // move left and into the screen
+
+            gl.Rotate(rtri, 0.0f, 1.0f, 0.0f);  // rotate pyramid on it's Y axis
+
+            gl.Begin(OpenGL.GL_TRIANGLES);      // start drawing the pyramid
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+
+            gl.End();
+
+            gl.LoadIdentity();
+            gl.Translate(1.5f, 0.0f, -7.0f);
+
+            gl.Rotate(rquad, 1.0f, 1.0f, 1.0f);
+
+            gl.Begin(OpenGL.GL_QUADS);
+
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+
+            gl.Color(1.0f, 0.5f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+
+            gl.Color(1.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+
+            gl.Color(1.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+
+            gl.End();
+
+            gl.Flush();
+
+            rtri = rtri + 3;
+            rquad = rquad + 3;
+        }
+    }
+}
